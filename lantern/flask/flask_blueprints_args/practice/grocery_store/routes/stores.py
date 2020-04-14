85ee8a574c1ae9d8bb36d0866c71359727ba3ec4
Igor Gavrylishyn <Ahiligor@gmail.com>
2020-04-14 16:56:22 +0300

    Finish Watch Lecture Sunday Blueprints

--- lantern/flask/flask_blueprints_args/practice/grocery_store/routes/stores.py
+++ lantern/flask/flask_blueprints_args/practice/grocery_store/routes/stores.py
@@ -1,14 +1,23 @@
 import inject
 from flask import request
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+parser = reqparse.RequestParser(bundle_errors=True)
+parser.add_argument('name', required=False, type=str)
 
 
 class Store(Resource):
 
-    def get(self, store_id):
+    def get(self, store_id=None):
         db = inject.instance('DB')
-        store = db.stores.get_store_by_id(store_id)
-        return store
+        if store_id is not None:
+            store = db.stores.get_store_by_id(store_id)
+            return store
+        else:
+            args = parser.parse_args()
+            name = args["name"]
+            store = db.stores.get_store_by_name(name)
+            return store
 
     def post(self):
         db = inject.instance('DB')
@@ -19,27 +28,23 @@ class Store(Resource):
         db = inject.instance('DB')
         db.stores.update_store_by_id(store_id, request.json)
         return {'status': 'success'}
-from flask import Blueprint, request, jsonify
 
-stores_bl = Blueprint("stores", __name__)
-
-
-@stores_bl.route('/store', methods=['POST'])
-def create_store():
-    db = inject.instance('DB')
-    store_id = db.stores.add(request.json)
-    return jsonify({'store_id': store_id}), 201
+# @stores_bl.route('/store', methods=['POST'])
+# def create_store():
+#     db = inject.instance('DB')
+#     store_id = db.stores.add(request.json)
+#     return jsonify({'store_id': store_id}), 201
 
 
-@stores_bl.route('/store/<int:store_id>')
-def get_store(store_id):
-    db = inject.instance('DB')
-    store = db.stores.get_store_by_id(store_id)
-    return jsonify(store)
+# @stores_bl.route('/store/<int:store_id>')
+# def get_store(store_id):
+#     db = inject.instance('DB')
+#     store = db.stores.get_store_by_id(store_id)
+#     return jsonify(store)
 
 
-@stores_bl.route('/store/<int:store_id>', methods=['PUT'])
-def update_store(store_id):
-    db = inject.instance('DB')
-    db.stores.update_store_by_id(store_id, request.json)
-    return jsonify({'status': 'success'})
+# @stores_bl.route('/store/<int:store_id>', methods=['PUT'])
+# def update_store(store_id):
+#     db = inject.instance('DB')
+#     db.stores.update_store_by_id(store_id, request.json)
+#     return jsonify({'status': 'success'})
