25dbc4554ad4571ae20fc5d18f529473fb1fcf8c
Igor Gavrylishyn <Ahiligor@gmail.com>
2020-05-07 20:08:16 +0300

    Flask HMW Done

--- lantern/grocery_store/fake_storage.py
+++ lantern/grocery_store/fake_storage.py
@@ -1,14 +1,24 @@
 from itertools import count
-from store_app import NoSuchUserError
+from store_app import NoSuchUserError, NoSuchStoreError, NoSuchManagerIDError
 
 
 class FakeStorage:
     def __init__(self):
         self._users = FakeUsers()
+        self._goods = FakeGoods()
+        self._stores = FakeStores()
 
     @property
     def users(self):
         return self._users
+
+    @property
+    def goods(self):
+        return self._goods
+
+    @property
+    def stores(self):
+        return self._stores
 
 
 class FakeUsers:
@@ -26,3 +36,69 @@ class FakeUsers:
             return self._users[user_id]
         except KeyError:
             raise NoSuchUserError(user_id)
+
+    def update_user_by_id(self, user_id, user):
+        if user_id in self._users:
+            self._users[user_id] = user
+        else:
+            raise NoSuchUserError(user_id)
+
+
+class FakeGoods:
+    def __init__(self):
+        self._goods = {}
+        self._id_counter = count(1)
+
+    def add(self, products):
+        good_qty = 0
+        for good in products:
+            good_id = next(self._id_counter)
+            good_qty += 1
+            self._goods[good_id] = good
+        return good_qty
+
+    def get_goods(self):
+        response = []
+        # import pdb;pdb.set_trace()
+        for key, values in self._goods.items():
+            response.append({"id": key, **values})
+        return response
+
+    def update_goods(self, goods):
+        updated = 0
+        no_id = []
+        for good in goods:
+            good_id = good['id']
+            if good_id in self._goods:
+                self._goods[good_id] = good
+                updated += 1
+            else:
+                no_id.append(good['id'])
+        return {'successfully_updated': updated, 'errors': {'no such id in goods': no_id}}
+
+
+class FakeStores:
+    def __init__(self):
+        self._stores = {}
+        self._id_counter = count(1)
+
+    def add(self, store):
+        try:
+            store_id = next(self._id_counter)
+            self._stores[store_id] = store
+            return store_id
+        except KeyError:
+            raise NoSuchUserError(store['manager_id'])
+
+    def get_store_by_id(self, store_id):
+        # import pdb;pdb.set_trace()
+        try:
+            return self._stores[store_id]
+        except KeyError:
+            raise NoSuchStoreError(store_id)
+
+    def update_store_by_id(self, store_id, store):
+        if store_id in self._stores:
+            self._stores[store_id] = store
+        else:
+            raise NoSuchStoreError(store_id)
--- lantern/grocery_store/store_app.py
+++ lantern/grocery_store/store_app.py
@@ -8,10 +8,30 @@ class NoSuchUserError(Exception):
         self.message = f'No such user_id {user_id}'
 
 
+class NoSuchStoreError(Exception):
+    def __init__(self, store_id):
+        self.message = f'No such store_id {store_id}'
+
+
+class NoSuchManagerIDError(Exception):
+    def __init__(self, manager_id):
+        self.message = f'No such store_id {manager_id}'
+
+
 app = Flask(__name__)
 
 
 @app.errorhandler(NoSuchUserError)
+def my_error_handler(e):
+    return jsonify({'error': e.message}), 404
+
+
+@app.errorhandler(NoSuchStoreError)
+def my_error_handler(e):
+    return jsonify({'error': e.message}), 404
+
+
+@app.errorhandler(NoSuchManagerIDError)
 def my_error_handler(e):
     return jsonify({'error': e.message}), 404
 
@@ -28,3 +48,53 @@ def get_user(user_id):
     db = inject.instance('DB')
     user = db.users.get_user_by_id(user_id)
     return jsonify(user)
+
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    db = inject.instance('DB')
+    db.users.update_user_by_id(user_id, request.json)
+    return jsonify({'status': 'success'})
+
+
+@app.route('/goods', methods=['POST'])
+def create_good():
+    db = inject.instance('DB')
+    good_qty = db.goods.add(request.json)
+    return jsonify({'numbers of items created': good_qty}), 201
+
+
+@app.route('/goods')
+def get_goods():
+    db = inject.instance('DB')
+    goods = db.goods.get_goods()
+    return jsonify(goods), 200
+
+
+@app.route('/goods', methods=['PUT'])
+def update_goods():
+    db = inject.instance('DB')
+    # import pdb;pdb.set_trace()
+    updated = db.goods.update_goods(request.json)
+    return jsonify(updated)
+
+
+@app.route('/stores', methods=['POST'])
+def create_store():
+    db = inject.instance('DB')
+    store_id = db.stores.add(request.json)
+    return jsonify({'store_id': store_id}), 201
+
+
+@app.route('/stores/<int:store_id>')
+def get_store(store_id):
+    db = inject.instance('DB')
+    store = db.stores.get_store_by_id(store_id)
+    return jsonify(store), 200
+
+
+@app.route('/stores/<int:store_id>', methods=['PUT'])
+def update_store(store_id):
+    db = inject.instance('DB')
+    db.stores.update_store_by_id(store_id, request.json)
+    return jsonify({'status': 'success'})
